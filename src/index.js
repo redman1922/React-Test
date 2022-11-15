f6d5ddb436cb6abb2cb470a11c4e9ff5d211d65f
Denis_Davidov <redman1922svap@mail.ru>
2022-11-15 16:55:41 +0300

    lesson-5
    lesson-5

--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {legacy_createStore} from "redux";
+import {Provider} from "react-redux";
+
+
+const initialState = {
+    showName: false,
+    name: 'Default'
+}
+
+const profileReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'True':
+            return {
+                ...state,
+                showName: !state.showName
+            }
+        default:
+            return state
+    }
+}
+
+
+const store = legacy_createStore(profileReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+            <App/>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
